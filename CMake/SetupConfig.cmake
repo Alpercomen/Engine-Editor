fba76bcebdba35fa3a9dc72cbef86987950c7979
alpercomen <comenalper@gmail.com>
2026-02-13 07:21:14 +0300

    CMake update

--- CMake/SetupConfig.cmake
+++ CMake/SetupConfig.cmake
@@ -1,13 +1,13 @@
 function(SetupConfig)
 	if(WIN32)
-		add_compile_definitions(SPACESIM_WINDOWS)
+		add_compile_definitions(AUDIOEDITOR_WINDOWS)
 	endif()
 
 	if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-		add_compile_definitions(SPACESIM_DEBUG)
+		add_compile_definitions(AUDIOEDITOR_DEBUG)
 	endif()
 
 	if(CMAKE_BUILD_TYPE STREQUAL "Release")
-		add_compile_definitions(SPACESIM_RELEASE)
+		add_compile_definitions(AUDIOEDITOR_RELEASE)
 	endif()
 endfunction()
